ebe718ad67e0bf169e5a09698d5f7c4fee30b700
peipeilee10 <vicky607vicky607@gmail.com>
2022-02-11 00:26:37 +0800

    appointment

--- src/views/front/Appointment.vue
+++ src/views/front/Appointment.vue
@@ -3,124 +3,278 @@
     <v-tabs v-model="tab" centered icons-and-text>
       <v-tabs-slider></v-tabs-slider>
 
-      <v-tab href="#tab1">
+      <v-tab href="#dog">
         狗狗
         <v-icon>mdi-dog</v-icon>
       </v-tab>
 
-      <v-tab href="#tab2">
+      <v-tab href="#cat">
         貓貓
         <v-icon>mdi-cat</v-icon>
       </v-tab>
     </v-tabs>
 
     <v-tabs-items v-model="tab">
-      <v-tab-item value="tab1">
-        <v-row dense class="mt-15 mb-5">
-          <v-col col="3"></v-col>
-          <v-col cols="3">
-            <v-card class="d-flex" max-width="400">
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-img
-                    height="250"
-                    max-width="400"
-                    src="../../assets/image/photoright.jpg"
-                  ></v-img>
+      <!-- 狗狗預約 -->
+      <v-tab-item value="dog">
+        <v-row dense class="mt-15 mb-5 flex-column">
+          <v-col>
+            <v-row dense>
+              <v-col cols="2"></v-col>
+              <v-col cols="8">
+                <!-- 卡片 -->
+                <v-card max-width="1200" class="appointmentcard">
+                  <v-row no-gutters>
+                    <v-col cols="4" class="text-center">
+                      <v-avatar class="ma-10" size="300" tile>
+                        <v-img
+                          src="../../assets/image/appointment.jpg"
+                          style="border-radius: 10px"
+                        ></v-img>
+                      </v-avatar>
+                    </v-col>
+                    <v-col cols="8" class="pr-15 pl-5 pt-5">
+                      <div class="item-title">
+                        <v-chip-group
+                          v-model="appointment.serviceitem"
+                          active-class="orange darken-1 white--text "
+                        >
+                          <v-chip label large :ripple="false" value="小美容">
+                            小美容
+                          </v-chip>
+                          <v-chip label large :ripple="false" value="大美容">
+                            大美容
+                          </v-chip>
+                        </v-chip-group>
+                        <p v-if="appointment.serviceitem === '小美容'">
+                          指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、小修剪足部、臉部腹部肛門雜毛
+                        </p>
+                        <p v-else>
+                          指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、 全身造型修剪
+                        </p>
+                      </div>
 
-                  <v-card-title class="text-h5">小美容</v-card-title>
-                  <v-card-subtitle>
-                    指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、小修剪足部、臉部腹部肛門雜毛
-                  </v-card-subtitle>
+                      <div class="pet-type">
+                        <span class="item-title">毛孩類型</span>
+                        <v-chip-group
+                          v-model="appointment.pettype"
+                          active-class="orange darken-1 accent-4 white--text"
+                        >
+                          <v-chip label value="小型犬">小型犬</v-chip>
+                          <v-chip label value="中型犬">中型犬</v-chip>
+                          <v-chip label value="大型犬">大型犬</v-chip>
+                        </v-chip-group>
+                      </div>
 
-                  <v-card-subtitle>毛孩類型</v-card-subtitle>
-                  <v-chip-group
-                    v-model="selection"
-                    active-class="deep-purple accent-4 white--text"
-                    column
+                      <v-divider class="py-2"></v-divider>
+
+                      <div class="booking-time">
+                        <span class="item-title">預約時段</span>
+                        <v-chip-group
+                          v-model="appointment.time"
+                          active-class="orange darken-1 accent-4 white--text"
+                        >
+                          <v-chip value="11:30">11:30</v-chip>
+                          <v-chip value="12:30">12:30</v-chip>
+                          <v-chip value="13:30">13:30</v-chip>
+                          <v-chip value="14:30">14:30</v-chip>
+                          <v-chip value="15:30">15:30</v-chip>
+                          <v-chip value="16:30">16:30</v-chip>
+                          <v-chip value="17:30">17:30</v-chip>
+                          <v-chip value="18:30">18:30</v-chip>
+                        </v-chip-group>
+                      </div>
+                    </v-col>
+                  </v-row>
+                  <!-- 價錢 -->
+                  <span class="price">
+                    <span
+                      v-if="
+                        appointment.serviceitem === '小美容' &&
+                        appointment.pettype === '小型犬'
+                      "
+                    >
+                      $ 800 / 1hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '小美容' &&
+                        appointment.pettype === '中型犬'
+                      "
+                    >
+                      $ 1,200 / 1hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '小美容' &&
+                        appointment.pettype === '大型犬'
+                      "
+                    >
+                      $ 1,600 / 1hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '大美容' &&
+                        appointment.pettype === '小型犬'
+                      "
+                    >
+                      $ 1,500 / 2hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '大美容' &&
+                        appointment.pettype === '中型犬'
+                      "
+                    >
+                      $ 2,400 / 2hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '大美容' &&
+                        appointment.pettype === '大型犬'
+                      "
+                    >
+                      $ 3,200 / 2.5hr
+                    </span>
+                  </span>
+                </v-card>
+                <div class="text-center my-6">
+                  <v-btn
+                    class="orange darken-1 white--text"
+                    depressed
+                    @click="ab"
+                    to="/appointmentCheckout"
                   >
-                    <v-chip>小型犬</v-chip>
-                    <v-chip>中型犬</v-chip>
-                    <v-chip>大型犬</v-chip>
-                  </v-chip-group>
-
-                  <v-divider></v-divider>
-
-                  <v-card-subtitle>預約時段</v-card-subtitle>
-                  <v-chip-group
-                    v-model="selection"
-                    active-class="deep-purple accent-4 white--text"
-                    column
-                  >
-                    <v-chip>11:30</v-chip>
-                    <v-chip>12:30</v-chip>
-                    <v-chip>13:30</v-chip>
-                    <v-chip>14:30</v-chip>
-                    <v-chip>15:30</v-chip>
-                    <v-chip>16:30</v-chip>
-                    <v-chip>17:30</v-chip>
-                    <v-chip>18:30</v-chip>
-                  </v-chip-group>
-
-                  <v-divider></v-divider>
-                  <v-card-subtitle>時間:1hr</v-card-subtitle>
-                  <v-card-subtitle>費用:$1,200</v-card-subtitle>
+                    前往預約
+                  </v-btn>
                 </div>
-              </div>
-            </v-card>
+              </v-col>
+              <v-col cols="2"></v-col>
+            </v-row>
           </v-col>
-          <v-col cols="3">
-            <v-card class="d-flex" max-width="400">
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-img
-                    height="250"
-                    max-width="400"
-                    src="../../assets/image/photoright.jpg"
-                  ></v-img>
+        </v-row>
+      </v-tab-item>
 
-                  <v-card-title class="text-h5">小美容</v-card-title>
-                  <v-card-subtitle>
-                    指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、小修剪足部、臉部腹部肛門雜毛
-                  </v-card-subtitle>
+      <!-- 貓咪預約 -->
+      <v-tab-item value="cat">
+        <v-row dense class="mt-15 mb-5 flex-column">
+          <v-col>
+            <v-row dense>
+              <v-col cols="2"></v-col>
+              <v-col cols="8">
+                <!-- 卡片 -->
+                <v-card max-width="1200" class="appointmentcard">
+                  <v-row no-gutters>
+                    <v-col cols="4" class="text-center">
+                      <v-avatar class="ma-10" size="300" tile>
+                        <v-img
+                          src="../../assets/image/appointmentcat.jpg"
+                          style="border-radius: 10px"
+                        ></v-img>
+                      </v-avatar>
+                    </v-col>
+                    <v-col cols="8" class="pr-15 pl-5 pt-5">
+                      <div class="item-title">
+                        <v-chip-group
+                          v-model="appointment.serviceitem"
+                          active-class="orange darken-1 white--text "
+                        >
+                          <v-chip label large :ripple="false" value="小美容">
+                            小美容
+                          </v-chip>
+                          <v-chip label large :ripple="false" value="大美容">
+                            大美容
+                          </v-chip>
+                        </v-chip-group>
+                        <p v-if="appointment.serviceitem === '小美容'">
+                          指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、小修剪足部、臉部腹部肛門雜毛
+                        </p>
+                        <p v-else>
+                          指甲修剪、清理耳朵、腹部剃毛、擠肛門腺、 全身造型修剪
+                        </p>
+                      </div>
 
-                  <v-card-subtitle>毛孩類型</v-card-subtitle>
-                  <v-chip-group
-                    v-model="selection"
-                    active-class="deep-purple accent-4 white--text"
-                    column
+                      <div class="pet-type">
+                        <span class="item-title">毛孩類型</span>
+                        <v-chip-group
+                          v-model="appointment.pettype"
+                          active-class="orange darken-1 accent-4 white--text"
+                        >
+                          <v-chip label value="長毛">長毛</v-chip>
+                          <v-chip label value="短毛">短毛</v-chip>
+                        </v-chip-group>
+                      </div>
+
+                      <v-divider class="py-2"></v-divider>
+
+                      <div class="booking-time">
+                        <span class="item-title">預約時段</span>
+                        <v-chip-group
+                          v-model="appointment.time"
+                          active-class="orange darken-1 accent-4 white--text"
+                        >
+                          <v-chip value="11:30">11:30</v-chip>
+                          <v-chip value="12:30">12:30</v-chip>
+                          <v-chip value="13:30">13:30</v-chip>
+                          <v-chip value="14:30">14:30</v-chip>
+                          <v-chip value="15:30">15:30</v-chip>
+                          <v-chip value="16:30">16:30</v-chip>
+                          <v-chip value="17:30">17:30</v-chip>
+                          <v-chip value="18:30">18:30</v-chip>
+                        </v-chip-group>
+                      </div>
+                    </v-col>
+                  </v-row>
+                  <!-- 價錢 -->
+                  <span class="price">
+                    <span
+                      v-if="
+                        appointment.serviceitem === '小美容' &&
+                        appointment.pettype === '短毛'
+                      "
+                    >
+                      $ 1,300 / 1hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '小美容' &&
+                        appointment.pettype === '長毛'
+                      "
+                    >
+                      $ 1,600 / 1.5hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '大美容' &&
+                        appointment.pettype === '短毛'
+                      "
+                    >
+                      $ 2,500 / 2hr
+                    </span>
+                    <span
+                      v-if="
+                        appointment.serviceitem === '大美容' &&
+                        appointment.pettype === '長毛'
+                      "
+                    >
+                      $ 2,800 / 2hr
+                    </span>
+                  </span>
+                </v-card>
+                <div class="text-center my-6">
+                  <v-btn
+                    class="orange darken-1 white--text"
+                    depressed
+                    @click="ab"
+                    to="/appointmentCheckout"
                   >
-                    <v-chip>小型犬</v-chip>
-                    <v-chip>中型犬</v-chip>
-                    <v-chip>大型犬</v-chip>
-                  </v-chip-group>
-
-                  <v-divider></v-divider>
-
-                  <v-card-subtitle>預約時段</v-card-subtitle>
-                  <v-chip-group
-                    v-model="selection"
-                    active-class="deep-purple accent-4 white--text"
-                    column
-                  >
-                    <v-chip>11:30</v-chip>
-                    <v-chip>12:30</v-chip>
-                    <v-chip>13:30</v-chip>
-                    <v-chip>14:30</v-chip>
-                    <v-chip>15:30</v-chip>
-                    <v-chip>16:30</v-chip>
-                    <v-chip>17:30</v-chip>
-                    <v-chip>18:30</v-chip>
-                  </v-chip-group>
-
-                  <v-divider></v-divider>
-                  <v-card-subtitle>時間:1hr</v-card-subtitle>
-                  <v-card-subtitle>費用:$1,200</v-card-subtitle>
+                    前往預約
+                  </v-btn>
                 </div>
-              </div>
-            </v-card>
+              </v-col>
+              <v-col cols="2"></v-col>
+            </v-row>
           </v-col>
-          <v-col col="3"></v-col>
         </v-row>
       </v-tab-item>
     </v-tabs-items>
@@ -132,23 +286,16 @@
     data() {
       return {
         tab: null,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        loading: false,
-        selection: 1,
-        items: [
-          {
-            color: '#1F7087',
-            src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-            title: 'Supermodel',
-            artist: 'Foster the People'
-          },
-          {
-            color: '#952175',
-            src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-            title: 'Halcyon Days',
-            artist: 'Ellie Goulding'
-          }
-        ]
+        appointment: {
+          serviceitem: '小美容',
+          pettype: '小型犬',
+          time: '11:30'
+        }
+      }
+    },
+    methods: {
+      ab() {
+        console.log(this.appointment)
       }
     }
   }
--- src/views/front/AppointmentCheckout.vue
+++ src/views/front/AppointmentCheckout.vue
@@ -1,1 +1,207 @@
-<!-- steppers -->
+<template>
+  <div class="container" id="appointmentCheckout">
+    <v-stepper v-model="e1">
+      <!-- 進度條 -->
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1">預約資訊</v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step :complete="e1 > 2" step="2">確認預約</v-stepper-step>
+      </v-stepper-header>
+
+      <!--內容區 -->
+      <v-stepper-items>
+        <!-- 預約資訊 -->
+        <v-stepper-content step="1">
+          <h1 class="text-center mb-15 mt-10">請填寫預約資訊</h1>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            @submit.prevent="submitorders"
+          >
+            <v-label for="name">預約人姓名</v-label>
+            <v-text-field
+              id="name"
+              v-model="form.name"
+              :rules="rules.nameRules"
+              placeholder="訂購人姓名"
+              required
+            ></v-text-field>
+
+            <v-label for="email">預約人電話</v-label>
+            <v-text-field
+              id="email"
+              v-model="form.email"
+              :rules="rules.emailRules"
+              placeholder="訂購人信箱"
+              required
+            ></v-text-field>
+
+            <v-label for="contact">預約人信箱</v-label>
+            <v-text-field
+              id="contact"
+              v-model="form.phone"
+              :rules="rules.phoneRules"
+              placeholder="訂購人聯絡電話"
+              required
+            ></v-text-field>
+
+            <v-label for="contact">毛孩名字</v-label>
+            <v-text-field
+              id="contact"
+              v-model="form.phone"
+              :rules="rules.phoneRules"
+              placeholder="訂購人聯絡電話"
+              required
+            ></v-text-field>
+
+            <v-label for="contact">毛孩品種</v-label>
+            <v-text-field
+              id="contact"
+              v-model="form.phone"
+              :rules="rules.phoneRules"
+              placeholder="訂購人聯絡電話"
+              required
+            ></v-text-field>
+
+            <v-label for="memo">備註：</v-label>
+            <v-textarea outlined id="memo" v-model="form.memo"></v-textarea>
+          </v-form>
+
+          <v-btn class="cart-btn" @click="e1 = 1" text>上一步</v-btn>
+
+          <v-btn class="cart-btn ml-10" @click="e1 = 3" type="submit" text>
+            下一步
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
+
+          <v-btn text>Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        e1: 1,
+        products: [],
+        headers: [
+          { text: '圖片', value: 'image' },
+          { text: '商品名', value: 'name' },
+          { text: '價格', value: 'price' },
+          { text: '操作', value: 'action' }
+        ],
+        form: {
+          name: '',
+          email: '',
+          address: '',
+          phone: '',
+          memo: '',
+          pay: '貨到付款',
+          pickupway: '宅配'
+        }
+      }
+    },
+    computed: {
+      rules() {
+        return {
+          nameRules: [v => !!v || '姓名必填'],
+          emailRules: [
+            v => !!v || '信箱必填',
+            v => /.+@.+\..+/.test(v) || '信箱格式錯誤'
+          ],
+          phoneRules: [v => !!v || '電話必填']
+        }
+      },
+      total() {
+        return this.products.reduce((accumulator, currentValue) => {
+          return (
+            accumulator + currentValue.quantity * currentValue.product.price
+          )
+        }, 0)
+      }
+    },
+    methods: {
+      async updateCart(index, quantity) {
+        try {
+          // 更新購物車
+          await this.api.patch(
+            'users/me/cart',
+            {
+              product: this.products[index].product._id,
+              quantity
+            },
+            {
+              headers: {
+                authorization: 'Bearer ' + this.user.token
+              }
+            }
+          )
+          // 刪除
+          if (quantity === 0) {
+            this.products.splice(index, 1)
+            this.$store.commit('user/updateCart', this.user.cart - 1)
+          }
+        } catch (error) {
+          this.$swal({
+            icon: 'error',
+            title: '失敗',
+            text: '修改購物車失敗'
+          })
+        }
+      },
+      async checkout() {
+        try {
+          await this.api.post('/orders', this.form, {
+            headers: {
+              authorization: 'Bearer ' + this.user.token
+            }
+          })
+          this.$swal({
+            icon: 'success',
+            title: '成功',
+            text: '已成功完成下定!'
+          })
+          this.$router.push('/back/member/orders')
+        } catch (error) {
+          this.$swal({
+            icon: 'error',
+            title: '失敗',
+            text: '結帳失敗'
+          })
+        }
+      }
+    },
+    async created() {
+      // 拿vuex資料
+      this.form.name = this.user.name
+      this.form.email = this.user.email
+      this.form.address = this.user.address
+      this.form.phone = this.user.phone
+      try {
+        const { data } = await this.api.get('users/me/cart', {
+          headers: {
+            authorization: 'Bearer ' + this.user.token
+          }
+        })
+        this.products = data.result
+      } catch (error) {
+        this.$swal({
+          icon: 'error',
+          title: '失敗',
+          text: '取得購物車失敗'
+        })
+      }
+    }
+  }
+</script>
